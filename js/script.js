37d8fa86fd8108f4404b6fbd8b2fa1b8dfeacc19
Lisa <lisa.muellner96@gmx.at>
2019-11-04 11:49:08 +0100

    implemented function for drawing the map of vienna

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
-function loadData() {
+function loadData(){
+    loadMapOfVienna();
+}
+
+function loadMapOfVienna(){
+
+    var svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+    // Projection
+    var projection = d3.geoMercator()
+        .scale(40000)
+        .center([16.5, 48.2])
+        .translate([ width/2, height/2 ]);
+
+    d3.json("./data/bezirksgrenzen.json").then(function(data){
+        console.log(data);
+
+        // Draw the map
+        svg.selectAll("path")
+            .data(data.features)
+            .enter()
+            .append("path")
+            .attr("fill", "grey")
+            .attr("d", d3.geoPath()
+                .projection(projection)
+            )
+            .attr('fill', 'none')
+            .attr('stroke', '#999999')
+            .attr('stroke-width', '0.5')
+    });
 }
