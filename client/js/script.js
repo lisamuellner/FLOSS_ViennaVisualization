a2ad074a466d9e469af645688035a70b5b5c56ed
Sarah <sarahannafisch@gmail.com>
2019-11-27 14:27:03 +0100

    small layout changes, tried to add charts, but there are some major performance issues

--- client/js/script.js
+++ client/js/script.js
@@ -1,5 +1,6 @@
 function loadData(){
     loadMapOfVienna();
+    loadCharts();
 }
 
 function loadMapOfVienna(){
@@ -9,7 +10,7 @@ function loadMapOfVienna(){
         centered,
         currentName;
 
-    const svg = d3.select("svg")
+    const svg = d3.select("#mapOfVienna")
         .attr("width", width)
         .attr("height", height)
         .attr("viewBox", "0 0 1000 500");
@@ -83,8 +84,9 @@ function loadMapOfVienna(){
                 paths.attr("d", path);
             };
 
+            // TODO: fix the performance issues, then change duration time back to 750
             d3.transition()
-                .duration(750)
+                .duration(0)
                 .tween("projection", function() {
                     return interpolator;
                 });
@@ -103,5 +105,54 @@ function loadMapOfVienna(){
                 .style('fill', 'green');
         }
     });
+}
+
+function loadCharts() {
+    /*
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+    var svg = d3.select("#chart1")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+
+    //Read the data
+    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv", function(data) {
+
+        // Add X axis
+        var x = d3.scaleLinear()
+            .domain([0, 4000])
+            .range([ 0, width ]);
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([0, 500000])
+            .range([ height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Add dots
+        svg.append('g')
+            .selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return x(d.GrLivArea); } )
+            .attr("cy", function (d) { return y(d.SalePrice); } )
+            .attr("r", 1.5)
+            .style("fill", "#69b3a2")
+
+    })*/
 }
 
