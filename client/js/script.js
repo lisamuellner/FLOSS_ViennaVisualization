188a4a57ad015dcda90ad9a64d5fc92dded230f9
Lisa <lisa.muellner96@gmx.at>
2020-01-08 14:03:14 +0100

    implemented functionality that general district information is shown

--- client/js/script.js
+++ client/js/script.js
@@ -21,6 +21,7 @@ async function loadDataForCharts(){
     //data loading complete - chartData is from here on available!
     fillCheckboxes();
     adaptSliderRangeToData();
+    showDistrictInformation();
     updateLineChart();
     updateBarChart();
     updateRadarChart();
@@ -175,6 +176,7 @@ function loadMapOfVienna(){
                     return interpolator;
                 });
             
+            showDistrictInformation();
             updateBarChart();
             updateLineChart();
             updateRadarChart();
@@ -353,6 +355,45 @@ function loadRadarChart(data, currentMaxValue){
     RadarChart("#chart3", data, radarChartOptions);
 }
 
+function showDistrictInformation(){
+    let districtCode; 
+    let filteredData = [];
+    if(selectedDistrict == 0){
+        let tmpFilteredData = chartData.filter(function(row){
+            return (row.REF_YEAR == selectedYear);
+        });
+        let areaSum = 0.0;
+        let populationSum = 0;
+        for(let i = 0; i < tmpFilteredData.length; i++){
+            areaSum += parseFloat(tmpFilteredData[i].AREA.replace(',', '.'));
+            populationSum += Number(tmpFilteredData[i].POP_TOTAL);
+        }
+        filteredData[0] = {};
+        filteredData[0].AREA = parseFloat(areaSum).toFixed(2);
+        filteredData[0].DISTRICT_BLAZON = "VIENNA_BLAZON.svg";
+        filteredData[0].DENSITY_OF_POPULATION = (populationSum/areaSum).toFixed(2);
+
+    }else{
+        if(selectedDistrict < 10){
+            districtCode = parseInt(("90" + selectedDistrict + "00"));
+        }else{
+            districtCode = parseInt(("9" + selectedDistrict + "00"));
+        }
+
+        filteredData = chartData.filter(function(row){
+            return (row.DISTRICT_CODE == districtCode && row.REF_YEAR == selectedYear);
+        });
+    }
+    
+    
+    let blazonImageName = filteredData[0].DISTRICT_BLAZON;
+    let area = filteredData[0].AREA; 
+    let densityOfPopulation = filteredData[0].DENSITY_OF_POPULATION;
+    document.getElementById("area").innerHTML = area + " km<sup>2</sup>";
+    document.getElementById("densityOfPopulation").innerHTML = densityOfPopulation + "/km<sup>2</sup";
+    document.getElementById("blazon").src = ("img/" + blazonImageName);
+}
+
 function updateLineChart () {
     let maxValue = 0;
     let districtCode;
@@ -378,9 +419,6 @@ function updateLineChart () {
     for(let y = 0; y < dataToProcess.length; y++) {
         let newEntry = {};
         newEntry["REF_YEAR"] = dataToProcess[y].REF_YEAR;
-
-        console.log("dataToProcess!!!");
-        console.log(dataToProcess);
 
         for (let i = 0; i < selectedDataSets.length; i++) {
             if (Number(dataToProcess[y][selectedDataSets[i]]) > maxValue) {
